fae263a8060544559d3fc3c046c89b71b62ede47
divy-dawda <divyadawda21@gmail.com>
2026-04-07 20:40:59 +0000

    update basics.cpp with new logic

--- sets/basics.cpp
+++ sets/basics.cpp
@@ -4,9 +4,24 @@ using namespace std;
 int main(){
     unordered_set<int> set;
     set.insert(1);
-    set.insert(1);
-    set.insert(1);
-    set.insert(1);
-    set.insert(1);
+    set.insert(2);
+    set.insert(3);
+    set.insert(4);
+    set.insert(5);
+    set.insert(1); // this wont be added to the set as set only contains unique values
+    cout<<set.size()<<endl; // prints 5 kyuki ek element ek baar hi add hoyega
+
+    // set.find() -> it searches in the set, and if it is not found then it returns the last element
+    int target = 41;
+    if(set.find(target) != set.end()){
+        cout<<"exists"<<endl;
+    }
+    else cout<<"doesn't exist"<<endl;
+
+    set.erase(2);
     
+    for(int element : set){
+        cout<<element<<" ";
+    }
+    cout<<endl;
 }
