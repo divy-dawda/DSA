64bfc15acd0ffd0f01caeb3ab764dd2d8286b9e6
divy-dawda <divyadawda21@gmail.com>
2026-06-11 18:07:05 +0000

    added function decimal to binary

--- bitManipulation/binaryToDecimal.cpp
+++ bitManipulation/binaryToDecimal.cpp
@@ -6,11 +6,29 @@ int binaryToDecimal(string& binary){
     for(int i = n - 1; i >= 0; i--){
         char ch = binary[i];
         int num = ch - '0';
-        result = result + num * (1 << (n - i - 1));
+        result = result + num * (1 << (n - i - 1)); // multiplication with 2 can be mimicked by left shift 1 (<< 1)
     }
     return result;
 }
+
+string decimalToBinary(int num){
+    string result = "";
+    while(num > 0){
+        if(num % 2 == 0){
+            result = '0' + result;
+        }
+        else{
+            result = '1' + result;
+        }
+        // num /= 2; 
+        // division by 2 can be mimicked right shift 1 (>> 1)
+        num = num >> 1;
+    }
+    return result;
+}
+
 int main(){
-    string str = "001101";
+    string str = "001011";
     cout << binaryToDecimal(str) << endl;
+    cout << decimalToBinary(19) << endl;
 }
